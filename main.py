d8dcbf242e84b085e504f0a7fe9596f8385ed339
miles-phillips0 <micpa42@gmail.com>
2022-09-22 12:05:54 -0400

    flask server running

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return flask.render_template("index.html")
+
+app.run(
+    host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "8080")), debug=True
+)
